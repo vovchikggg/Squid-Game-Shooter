5e1814e21fa304ef4d776e0024862cd7c40dc52e
vovchikggg <vl_gavrikov@mail.ru>
2025-04-06 19:29:10 +0500

    Add bots and mega AI

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SquidGameShooter.Utils
+{
+    public static class Utils
+    {
+        public static Vector3 GetRandomDir()
+        {
+            return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        }
+    }
+}
