5ec43129a095d1d325cc170e868f51714b4568b7
vovchikggg <vl_gavrikov@mail.ru>
2025-06-01 00:27:54 +0500

    pochti inv

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -8,7 +8,55 @@ public class Inventory : MonoBehaviour
     {
         Instance = this;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            activeSlot = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            activeSlot = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            activeSlot = 2;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            var inventorySlotChild = default(GameObject);
+            
+            foreach (Transform child in slots[activeSlot].transform)
+            {
+                inventorySlotChild = child.gameObject;
+            }
+            var weaponSlotChild = default(GameObject);
+            
+            foreach (Transform child in weaponSlot.transform)
+            {
+                weaponSlotChild = child.gameObject;
+            }
+            
+            (inventorySlotChild, weaponSlotChild) = (weaponSlotChild, inventorySlotChild);
+
+            foreach (Transform child in slots[activeSlot].transform)
+            {
+                Destroy(child.gameObject);
+            }
+            
+            foreach (Transform child in weaponSlot.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            if (inventorySlotChild)
+                Instantiate(inventorySlotChild, slots[activeSlot].transform);
+            
+            if (weaponSlotChild)
+                Instantiate(weaponSlotChild, weaponSlot.transform);
+        }
+    }
     
+    public bool weaponSlotIsFull;
     public bool[] isFull;
+    public GameObject weaponSlot;
     public GameObject[] slots;
+    
+    public int activeSlot;
 }
---
+++ Assets/Scripts/Inventory/Use.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Use : MonoBehaviour
+{
+    [SerializeField] public GameObject icon;
+
+    public void UseItem()
+    {
+        ChangeWeapon();
+    }
+
+    private void ChangeWeapon()
+    {
+        // (inventory.isFull[inventory.activeSlot], inventory.weaponSlotIsFull) =
+        //     (inventory.weaponSlotIsFull, inventory.isFull[inventory.activeSlot]);
+        // Instantiate(icon, inventory.weaponSlot.transform);
+        // Destroy(gameObject);
+    }
+}
