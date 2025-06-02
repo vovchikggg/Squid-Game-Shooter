76860b6682604bca7474f273b5463a815f1e16cc
vovchikggg <vl_gavrikov@mail.ru>
2025-06-02 18:36:11 +0500

    add slides and music

---
+++ Assets/Scripts/SlidesManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SlidesManager : MonoBehaviour
+{
+    public static SlidesManager Instance;
+
+    [SerializeField] private GameObject[] StorySlides;
+    [SerializeField] private GameObject TipsSlide;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
